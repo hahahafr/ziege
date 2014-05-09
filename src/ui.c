c3b895dd53cbdd9c1d2f473e470c6cdf8013ec5e
hahahafr <ullrich.mailbox@gmail.com>
2014-05-10 01:07:24 +0200

    un début d'interface divisé en 3 (pas testé)

--- src/ui.c
+++ src/ui.c
@@ -3,16 +3,29 @@
 int
 init_affichage(jeux_s jeu)
 {
+    int screeny, screenx;
+    int ch;
+    
     initscr();
-    printw("Hi you.");
+    cbreak();
+    noecho();
+    
+    getmaxyx(stdscr, screeny, screenx);
+    
+    WINDOW *etat = newwin(20, screeny/2, 0, screenx-20);
+    WINDOW *cimetiere = newwin(20, screeny/2, screenx/2, screenx-20);
+    WINDOW *plateau = newwin(49, 29, (screeny-49)/2, ((screenx-29)/2));
+    
     refresh();
-    getch();
+    
+    wgetch();
     endwin();
-
+    
     return 0;
 }
 
-int maj_affichage(jeux_s jeu)
+int
+maj_affichage(jeux_s jeu)
 {
     return 0;
 }
